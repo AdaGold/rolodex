4007e48669afe57e500b45abc306a46bd334f6b6
Olivia <reach.olivialegge@gmail.com>
2016-11-29 16:42:44 -0800

    wave 1, updated with model interacting with view

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -6,12 +6,8 @@ import Contact from 'app/models/contact.js';
 
 const ContactView = Backbone.View.extend({
   initialize: function(options){
-    // this.model = options.model
-    this.model = {
-      name: "Justin Bieber",
-      email: "imma@belieber.com",
-      phone: "585-507-3425"
-    }
+    this.model = options.model
+
     this.template = _.template($('#tmpl-contact-card').html());
     this.listElement = this.$('.contact-card');
   },
@@ -20,10 +16,8 @@ const ContactView = Backbone.View.extend({
     this.listElement.empty();
 
 
-    var html = this.template(this.model);
+    var html = this.template(this.model.toJSON());
     this.$el.html(html);
-    // this.listElement.append(html);
-
 
     return this;
   },
