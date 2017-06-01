4219f9e01c0e52dd277fecc6136515579f5ef007
janicewilson <janice.wilson@mac.com>
2017-06-01 16:48:26 -0700

    Added constructor model for contact.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+import _ from 'underscore';
 import $ from 'jquery';
+import Contact from './app/models/contact.js'
+import Yolodex from './app/collections/yolodex.js'
+
+var contact = new Contact({
+     name: 'Princess Leia',
+     email: 'princess@therebellion.com',
+     phone: '+3 (333) 333-3333'
+});
+
+// var myTask = new Task({
+//      title: 'Create a Model.',
+//      description: 'Extend Backbone.Model.',
+//      completed: false
+// });
+
+var render = function(contact) {
+     var template_text = $('#tmpl-contact-details').html();
+     var templateObject = _.template(template_text);
+     var compiledHTML = templateObject(contact.toJSON());
+     $('main').append(compiledHTML);
+};
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,8 +1,11 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
-  // This model should have the attributes for
-  // a single contact: name, phone number, and email.
+     defaults: {
+       name: '',
+       email: '',
+       phone: ''
+     }
 });
 
 export default Contact;
